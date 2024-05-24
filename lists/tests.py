a151feb408464ca953585a02788b2dffc8e17eb6
0 superminivan <943983214@qq.com>
2024-05-24 14:15:14 +0800

    message 7: Refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -5,10 +5,10 @@ from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found=resolve('/')
-        self.assertEqual(found.func, home_page)
 
+    def test_uses_home_template(self):
+        response=self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
     def test_home_page_return_correct_html(self):
         request=HttpRequest()
         response=home_page(request)

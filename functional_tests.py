4b71f23bf18dff8ff02c0dbe7f93806f810c8d23
0 superminivan <943983214@qq.com>
2024-05-24 14:34:37 +0800

    message 8 :Before refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Chrome()
     def tearDown(self):
         self.browser.quit()
+    def check_for_row_in_list_table(self,row_text):
+        table=self.browser.find_element(By.ID,'id_list_table')
+        rows=table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 张三听说有一个在线待办事项的应用
         #他去看了这个应用的首页
@@ -34,10 +39,7 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项表格中显示了“1: Buy flowers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1: Buy flowers',[row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy flowers')
 
         # 页面中又显示了一个文本输入框，可以输入其他待办事项
         # 他又输入了“Give a gift to Lisi"
@@ -45,11 +47,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+
         # 页面再次更新，他的清单中显示了这两个待办事项
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1: Buy flowers',[row.text for row in rows])
-        self.assertIn('2: Give a gift to Lisi',[row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy flowers')
+        self.check_for_row_in_list_table('2: Give a gift to Lisi')
         # 张三想知道这个网站是否会记住他的清单
         # 他看到网站为他生成了一个唯一的URL
 

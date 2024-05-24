a28924d1d2433b679ec6cedaa5ceb00aa31484bb
0 superminivan <943983214@qq.com>
2024-05-24 14:32:09 +0800

    message 8 :Before refactoring the functional_tests

--- lists/tests.py
+++ lists/tests.py
@@ -9,12 +9,15 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response=self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-    def test_home_page_return_correct_html(self):
-        request=HttpRequest()
-        response=home_page(request)
-        html=response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-
+    # def test_home_page_return_correct_html(self):
+    #     request=HttpRequest()
+    #     response=home_page(request)
+    #     html=response.content.decode('utf8')
+    #     self.assertTrue(html.startswith('<html>'))
+    #     self.assertIn('<title>To-Do lists</title>', html)
+    #     self.assertTrue(html.endswith('</html>'))
+    def test_can_save_a_POST_request(self):
+        response=self.client.post('/', data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 # Create your tests here.

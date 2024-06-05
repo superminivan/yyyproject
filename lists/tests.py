c7b71366e07eeccf9c812ffd19be027244db4bd6
0 superminivan <943983214@qq.com>
2024-06-05 15:57:42 +0800

    message 16: form URL of new list by POST

--- lists/tests.py
+++ lists/tests.py
@@ -22,22 +22,8 @@ class ItemModelTest(TestCase):
         second_saved_item=saved_items[1]
         self.assertEqual(first_saved_item.text,'The first list item')
         self.assertEqual(second_saved_item.text,'Item the second')
-    def test_can_save_a_POST_request(self):
-        response=self.client.post('/',data={'item_text':'A new list item'})
-
-        self.assertEqual(Item.objects.count(),1)
-        new_item=Item.objects.first()
-        self.assertEqual(new_item.text,'A new list item')
-
-    def test_redirects_after_POST(self):
-        response=self.client.post('/',data={'item_text':'A new list item'})
-        self.assertEqual(response.status_code,302)
-        self.assertEqual(response['location'],'/lists/the-new-page/')
 
 
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(),0)
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -60,3 +46,12 @@ class ListViewTest(TestCase):
         self.assertContains(response,'itemey 1')
         self.assertContains(response,'itemey 2')
 # Create your tests here.
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertEqual(Item.objects.count(),1)
+        new_item=Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+    def test_redirects_after_POST(self):
+        response=self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertRedirects(response,'/lists/the-new-page/')
